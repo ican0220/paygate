0a983f07a0402a0cce2cb16f22468f1c62ae8e2e
Ican <icandev0220@gmail.com>
2024-09-18 00:42:38 +0900

    pay page added

---
+++ src/app/pay/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface InvoiceComponentProps {
+
+}
+
+const InvoiceComponent: React.FC<InvoiceComponentProps> = ({}) => {
+    return (
+        <div>
+            Hello, this is pay page.
+        </div>
+    )
+}
+
+export default InvoiceComponent;
